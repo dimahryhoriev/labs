8a5b2574f0fdb3df88acbf82640fd8f065fba412
Dmytro Hryhoriev <dimag4888@gmail.com>
2026-04-19 20:01:00 +0200

    feat(eloquent-js): complete looping a triangle exercise

---
+++ learning/books/eloquent-javascript-haverbeke/01-triangle-loop/task.js
@@ -0,0 +1,20 @@
+/* TASK:
+---
+    Write a loop that makes seven calls to
+    console.log to output the following triangle:
+    #
+    ##
+    ###
+    ####
+    #####
+    ######
+    #######
+---
+*/
+
+let triangle = '';
+
+while (triangle.length < 7) {
+    triangle += '#'
+    console.log(triangle)
+}
