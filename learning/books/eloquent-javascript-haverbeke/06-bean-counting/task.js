603abeb899cc85d2faa42a44c8e0f9c9dbdd8031
Dmytro Hryhoriev <dimag4888@gmail.com>
2026-05-01 19:01:07 +0300

    feat(eloquent-js): create countBs function for first part of Bean Counting exercise

---
+++ learning/books/eloquent-javascript-haverbeke/06-bean-counting/task.js
@@ -0,0 +1,39 @@
+/* TASK: Bean Counting
+
+You can get the Nth character of a string by adding .charAt(N) to it 
+("string".charAt(5)) – similar to getting the length of a string 
+using .length. The returned value will be a string consisting of 
+a single character (for example, "k"). The first character of a string 
+has position 0, which means the last character's position will be 
+string.length – 1. In other words, a two-character string has 
+length 2, and its characters' positions are 0 and 1.
+
+Write a function countBs that takes a string as an argument 
+and returns the number of "B" characters contained in the string.
+
+Next, write a function countChar that works similarly to countBs, 
+except it takes a second parameter — the character we will be 
+looking for in the string (instead of just counting "B" characters). 
+To do this, rewrite the countBs function.
+
+*/
+
+
+function countBs(string) {
+    let numberOfBs = 0;
+    let stringLength = string.length;
+
+    function verifyChar(stringLength) {
+        let updatedLength = stringLength - 1;
+        let lastChar = string.charAt(updatedLength);
+
+        if (updatedLength < 0) return numberOfBs;
+        if (lastChar === 'B') numberOfBs++;
+
+        return verifyChar(updatedLength);
+    }
+
+    return verifyChar(stringLength);
+}
+
+console.log(countBs('ABABBB'));
